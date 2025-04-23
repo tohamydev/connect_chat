e40a841ac210c41aa476097213671d22e5ff29d8
Mohammed El-tohamy <mohtohamy@inbox.ru>
2025-04-23 15:43:55 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/bconnect/chat/b_connect_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bconnect.chat.b_connect_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatApp());
+
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
